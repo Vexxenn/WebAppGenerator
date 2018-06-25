6dd26ebef8529eb91656676c95656c26cdc3c266
Jorge Moreira <32908579+Vexxenn@users.noreply.github.com>
2018-06-25 10:30:24 +0100

    Fixed insert M-M hopefuly

--- Publish/Models/Product.js
+++ Publish/Models/Product.js
@@ -43,12 +43,13 @@ Product.prototype.save = function (checkboxValues, callback) {
                 var tablerOrder = ["Product", relationMToM()];
                 tablerOrder.sort();
                 tablerOrder = tablerOrder.join('_');
-
+                var someObject = {Sale_id : ""};
                 for(let i = 0; i < checkboxValues.length; i++){
                     if(tablerOrder.split("_")[0] == "Product"){
-                        database.run("SELECT Product_id FROM Products ORDER BY Product_id DESC LIMIT 1", [], function(id){
+                        database.get("SELECT Product_id FROM Products ORDER BY Product_id DESC LIMIT 1", [],Product, function(id){
                             //Retorna o id a false apesar da query funcionar propriamente (query foi testada no DB Browser)
-                            console.log(id)
+                            
+                            console.log(id["Product_id"])
                             console.log("INSERT INTO " + tablerOrder + " VALUES (" + id + "," +checkboxValues[i]+ ")");
                             database.run("INSERT INTO " + tablerOrder + " VALUES (?,?)", [id, checkboxValues[i]]);
                         })
